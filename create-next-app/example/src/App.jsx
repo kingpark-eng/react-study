fb37e6ef59d1abbb25067ecb83995c5851885fbc
kingpark-eng <qkr12294@gmail.com>
2025-11-13 10:11:57 +0900

    App.jsx update

--- create-next-app/example/src/App.jsx
+++ create-next-app/example/src/App.jsx
@@ -3,37 +3,36 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const Square = ()=>{
-
-  const [value, setValue] = useState(null);
-
-  const handleClick = ()=>{
-    setValue('X');
-  }
-
-  return <button className="square" onClick={handleClick}>{value}</button>;
+const Square = ({value, onSquareClick})=>{
+  return (<button className="square" onClick={onSquareClick}>{value}</button>);
 }
 
 export default function App(){
 
-  const [count, setCount] = useState(0);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const onSquareClick=(i)=>{
+    const nextSquares = squares.slice();
+    nextSquares[i]="X";
+    setSquares(nextSquares);
+  } 
 
   return (
     <>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[0]} onSquareClick={handleClick}/>
+        <Square value={squares[1]} onSquareClick={handleClick}/>
+        <Square value={squares[2]} onSquareClick={handleClick}/>
       </div>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[3]} onSquareClick={handleClick}/>
+        <Square value={squares[4]} onSquareClick={handleClick}/>
+        <Square value={squares[5]} onSquareClick={handleClick}/>
       </div>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[6]} onSquareClick={handleClick}/>
+        <Square value={squares[7]} onSquareClick={handleClick}/>
+        <Square value={squares[8]} onSquareClick={handleClick}/>
       </div>
     </>
   );
